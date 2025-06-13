8721c0e86d0128b59bc4e215115e6c5db00c948e
Sakata Gintoki <yogeshsewada0@gmail.com>
2025-06-13 19:42:45 +0530

    command not found implimented

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  // Flush after every cout / std:cerr
+  cout << unitbuf;
+  cerr << unitbuf;
 
-  std::cout << "$ ";
+  cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
+  string input;
+  getline(cin, input); 
+	cout << input << ": command not found" << endl; 
+
+
+return 0;
 }
--- yshell.sh
+++ yshell.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-#
-# Use this script to run your program LOCALLY.
-#
-# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
-#
-# Learn more: https://codecrafters.io/program-interface
 
 set -e # Exit early if any commands fail
 
-# Copied from .codecrafters/compile.sh
 #
 # - Edit this to change how your program compiles locally
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
