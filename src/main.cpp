0dbdf963b69a26ba7715135b91c0b7e275133600
Sakata Gintoki <yogeshsewada0@gmail.com>
2025-06-14 16:41:43 +0530

    tokeniser and parser added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+vector<string> tokeniser(string input) {
+  vector<string> tokens;
+
+  string token = "";
+  for (int i = 0; i < input.size(); i++) {
+    if (input[i] == ' ') {
+      tokens.push_back(token);
+      token = "";
+    } else if (i == input.size() - 1) {
+      token += input[i];
+      tokens.push_back(token);
+    } else {
+      token += input[i];
+    }
+  }
+
+  return tokens;
+}
+
+void printError(string cmd, string message) {
+  cout << cmd << ": " << message << endl;
+}
+
+void handleEcho(vector<string> tokens) {
+  string statement = "";
+  for (int i = 1; i < tokens.size(); i++) {
+    statement += tokens[i];
+    if (i != tokens.size() - 1) {
+      statement += " ";
+    }
+  }
+
+  cout << statement << endl;
+}
+
+void exit() { exit(0); }
+
+void parser(vector<string> tokens) {
+  string keyword = tokens[0];
+
+  if (keyword == "echo") {
+    handleEcho(tokens);
+  } else if (keyword == "exit") {
+    exit();
+  } else {
+    printError(keyword, "command not found");
+  }
+}
 
 int main() {
   // Flush after every cout / std:cerr
@@ -15,11 +65,8 @@ int main() {
     string input;
     getline(cin, input);
 
-    if (input == "exit 0") {
-      exit(0);
-    } else {
-      cout << input << ": command not found" << endl;
-    }
+    vector<string> tokens = tokeniser(input);
+    parser(tokens);
   }
 
   return 0;
